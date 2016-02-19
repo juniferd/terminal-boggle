d9f22fc88f723c2776b4949ed8b8176159b42c68
juniferd <jyk@jyk>
2016-02-19 13:02:38 -0800

    added basic scoring

--- engine.py
+++ engine.py
@@ -8,7 +8,7 @@ class Engine(object):
     def __init__(self):
         self.__timer_on = True
         self.__remaining_time = 60
-
+        self.__round_score = 0
 
     """
     menu_map = {
@@ -28,27 +28,54 @@ class Engine(object):
         ## FIX THIS LATER
         if player_input == 's':
             self.start_round()
+        elif player_input == 'e':
+            self.stop_round()
 
     # start a round
     def start_round(self):
-        round_score = 0
-        
+        ##FIX THIS LATER
         game_words = self.play_game()
-        print "Here are your words from the game: ", game_words
-        # game needs to return player's inputs with True/False if valid
-        self.game_reset()
+        print "Here are your words from the game: "
+        for word in game_words:
+            print word
+        
+        game_score = self.score_game(game_words)
         # score a game
-
+        print "Your game score: ", game_score
         # add score to round_score
+
+        self.__round_score += game_score
+        print "Your round score: ", self.__round_score
+        self.game_reset()
 
         # present the menu
         self.menu()
     
     # manually stop a round
-
+    def stop_round(self):
+        self.__round_score = 0
+        self.menu()
     # change board to size nxn
 
-    # scoreboard
+    # score
+    def score_game(self, words):
+        scoring = {
+            3 : 1,
+            4 : 1,
+            5 : 2,
+            6 : 3,
+            7 : 5,
+            8 : 11
+        }
+        score = 0
+        for word in words:
+            length = int(words[word])
+            if length >= 8:
+                score = score + 11
+            else:
+                score = score + scoring[length]
+
+        return score
 
     def game_reset(self):
         
@@ -81,6 +108,6 @@ class Engine(object):
 
             this_word = mechanics.Boggle().check_word(user_word.lower(), board)
             if this_word is not None:
-                checked_words[this_word] = this_word
+                checked_words[this_word] = len(this_word)
 
         return checked_words
